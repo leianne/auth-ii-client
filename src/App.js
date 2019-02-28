6efab8ecfb20990efe54b86486442490e4c1b3e3
leianne louis <leilouis112@gmail.com>
2019-02-28 16:25:13 -0500

    completed basic redux react setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, NavLink } from 'react-router-dom';
+import UserView from './views/UserView';
+import LoginView from './views/LoginView';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <>
+        <header>
+          <NavLink to='/users'>Users</NavLink>
+          <NavLink to='/login'>Login</NavLink>
+          <NavLink to='register'>Register</NavLink>
         </header>
-      </div>
+        <Route to='/users' component={UserView} />
+        <Route to='/users' component={LoginView} />
+      </>
     );
   }
 }
